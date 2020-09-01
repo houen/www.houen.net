905d7938f3c703f8733287a2c68f9e41b48112b8
Søren Houen <s@houen.net>
2020-09-01 09:47:41 +0200

    add doctoc script

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# TODO: doctoc
+_bin/doctoc.sh
+git commit -m "update TOC" _pages/work-guide.md
+
 # TODO: pull work_guide/README.md from Dropbox/GitHub and link
 # TODO: pull and link CV-condensed.md from Dropbox
 
