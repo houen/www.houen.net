fcb4806f5b7fc7838df99a4bc054ea926607df54
Søren Houen <s@houen.net>
2020-10-15 18:55:02 +0200

    reboot docker on deploy

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -13,11 +13,9 @@
 # https://bobcares.com/blog/auto-restart-docker-containers/2/#:~:text=To%20set%20a%20restart%20policy,docker%20daemon%20would%20restart%20it.
 # https://docs.docker.com/engine/reference/run/#restart-policies---restart
 
-# jekyll build
-#scp -r _bin/on_rpi_server/* rpi.houen.net:/home/houen/www.houen.net/bin/.
+jekyll build
 rsync --recursive --delete _bin/on_rpi_server rpi.houen.net:/home/houen/www.houen.net/bin
-# scp -r _site/* rpi.houen.net:/home/houen/www.houen.net/site/.
 rsync --recursive --delete _site rpi.houen.net:/home/houen/www.houen.net/site
-# ssh rpi.houen.net "docker stop www_houen_net"
-# ssh rpi.houen.net "docker rm www_houen_net"
-# ssh rpi.houen.net "sh /home/houen/www.houen.net/bin/docker_start.sh"
+ssh rpi.houen.net "docker stop www_houen_net"
+ssh rpi.houen.net "docker rm www_houen_net"
+ssh rpi.houen.net "sh /home/houen/www.houen.net/bin/docker_start.sh"
