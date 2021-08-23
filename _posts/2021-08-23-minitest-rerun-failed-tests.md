83b38e2076ea5b444478469f04b61998199fab4e
Søren Houen <s@houen.net>
2021-08-23 11:25:54 +0200

    add minitest rerun failed post

---
+++ _posts/2021-08-23-minitest-rerun-failed-tests.md
@@ -0,0 +1,40 @@
+---
+layout: post
+title: Minitest Rerun failed tests
+description: Easily rerun failed tests with this Minitest reporter
+summary: Easily rerun failed tests with this Minitest reporter
+tags: [ruby, testing, minitest]
+---
+
+I have started using [Minitest](https://github.com/seattlerb/minitest) instead of [RSpec](https://github.com/rspec/rspec) for my Rails testing.
+
+The learning curve was a bit tricky, since they approach testing in somewhat different ways. But Minitest is a joy to use once I got the hang of it. I dont even miss `let!`'s at all. In retrospect, I had picked up some bad testing habits from RSpec. Now my tests are much simpler and more verbose. Thanks to Minitest and [Ryan Davis](https://www.zenspider.com/) they are also faster.
+
+## Rerunning failed tests easily with Minitest
+
+One thing I was missing in Minitest from RSpec was a way to very easily rerun just my failed tests from the last test run. 
+
+So I wrote a custom reporter based on [Minitest-reporters](https://github.com/minitest-reporters/minitest-reporters) to do just that.
+
+It should really be a gem instead, and I might get around to "gemify" it at a later point.
+
+## How to rerun failed tests
+
+I add the [file below](#minitest-failed-tests-reporter-code) to a project and include it with Minitest-reporters like this:
+
+```ruby
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::FailedTestsReporter.new(verbose: true, include_line_numbers: true)]
+```
+
+I can now rerun only failed tests with:
+
+```
+bundle exec rails test $(cat .minitest_failed_tests.txt)
+```
+
+## Minitest failed tests reporter code
+
+**test/support/minitest/reporters/failed_tests_reporter.rb**
+```ruby
+{% include_relative 2021-08-23-minitest-rerun-failed-tests/failed_tests_reporter.rb %}
+```
---
+++ _posts/2021-08-23-minitest-rerun-failed-tests/failed_tests_reporter.rb
@@ -0,0 +1,127 @@
+module Minitest
+    module Reporters
+      # Outputs failed tests to screen and / or file
+      # Allows to rerun only failed tests with minitest if added to Minitest::Reporters.use!
+      #
+      # Example:
+      #   In test_helper.rb or similar:
+      #   Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::FailedTestsReporter.new(verbose: true, include_line_numbers: true)]
+      #
+      #   Now after a failed test run, rerun failed tests only with: `bundle exec rails test $(cat .minitest_failed_tests.txt)`
+      #
+      class FailedTestsReporter < BaseReporter
+  
+        def initialize(options = {})
+          super(options)
+  
+          # Output to console?
+          @verbose              = options.fetch(:verbose, true)
+          # Output to file?
+          @file_output          = options.fetch(:file_output, true)
+          # What path to file?
+          @output_path          = options.fetch(:output_path, '.')
+          # Include line numbers? (failed_test.rb:42 or just failed_test.rb)
+          @include_line_numbers = options.fetch(:include_line_numbers, true)
+  
+          @output_file_path     = File.join(@output_path, '.minitest_failed_tests.txt')
+        end
+  
+        def record(test)
+          tests << test
+        end
+  
+        def report
+          super
+          failure_paths      = []
+          file_output_str = ''
+          curdir = FileUtils.pwd
+  
+          tests.each do |test|
+            next if test.skipped?
+            next unless test.failure.present?
+  
+            # DEBUG OUTPUT STR
+            # p '============================================='
+            # p "Failure:\n#{test.class}##{test.name} [#{test.failure.location}]\n#{test.failure.class}: #{test.failure.message}"
+            # p '============================================='
+  
+            # Build a haystack string from failures and errors to find test file location in
+            tmp_haystack_str = test.failure.location
+            tmp_haystack_str += test.to_s
+            unless test.failure.is_a?(MiniTest::UnexpectedError)
+              trace = filter_backtrace(test.failure.backtrace)
+              trace.each do |line|
+                # Add text with the correct failure line location for FAIL results
+                tmp_haystack_str += line
+              end
+            end
+  
+            # Get file name and optionally line number from haystack
+            if @include_line_numbers
+              regex = /\s*(.+_test\.rb:[0-9]+)/
+            else
+              regex = /\s*(.+_test\.rb):[0-9]+/
+            end
+  
+            # Failure location as best we can from haystack
+            failure_file_location = tmp_haystack_str.match(regex)&.to_a&.fetch(1, nil)
+            # Skip to next if we could not find anything
+            next unless failure_file_location
+            # Make path relative if absolute
+            failure_file_location = failure_file_location.gsub(curdir, '')
+            failure_file_location = failure_file_location.gsub(/^\//,'')
+  
+            # Store for outputting
+            failure_paths << failure_file_location if failure_file_location
+          end
+  
+          # OUTPUT RESULTS
+          if failure_paths.present?
+            _puts("")
+            headline = @include_line_numbers ? 'Failed tests:' : 'Failed test files:'
+            _puts(headline)
+            failure_paths.uniq.each do |file_path|
+              file_output_str << "#{file_path}\n"
+              _puts red(file_path)
+            end
+          end
+  
+          File.write(@output_file_path, file_output_str, encoding: 'UTF-8')
+        end
+  
+        private
+  
+        def _puts(str)
+          return unless @verbose
+          puts(str)
+        end
+  
+        def print_padded_comment(line)
+          puts "##{pad(line)}"
+        end
+  
+        def color?
+          return @color if defined?(@color)
+          @color = @options.fetch(:color) do
+            io.tty? && (
+              ENV["TERM"] =~ /^screen|color/ ||
+                ENV["EMACS"] == "t"
+            )
+          end
+        end
+  
+        def green(string)
+          color? ? ANSI::Code.green(string) : string
+        end
+  
+        def yellow(string)
+          color? ? ANSI::Code.yellow(string) : string
+        end
+  
+        def red(string)
+          color? ? ANSI::Code.red(string) : string
+        end
+      end
+    end
+  end
+  
